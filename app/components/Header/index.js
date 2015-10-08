47ed6e822838428506a38a0228eca0f74e18c953
Kevin Simper <kevin.simper@gmail.com>
2015-10-09 01:11:34 +0200

    Places a create project button in the header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import Button from '../Button'
 import './style.scss'
 
 export default class Header extends React.Component {
+  onClickCreate() {
+    alert('Create!')
+  }
   render() {
     return (
       <div className='Header'>
-        Kabys
+        <div className='Header__Title'>
+          Kabys
+        </div>
+        <div className='Header__Settings'>
+          <Button onClick={this.onClickCreate}>Create Project</Button>
+        </div>
       </div>
     )
   }
