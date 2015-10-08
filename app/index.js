8758893250816caa07b5dcba665df0a45bc2cfd0
Kevin Simper <kevin.simper@gmail.com>
2015-10-08 15:28:18 +0200

    Detect if there is a dockerfile in between the dropped files

--- app/index.js
+++ app/index.js
@@ -7,20 +7,33 @@ class Kabys extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      files: []
+      files: [],
+      dockerfile: false
     }
-    dragDrop('body', files => {
-      console.log(files)
-      this.setState({
-        files: files
-      })
+    dragDrop('body', this.onDragged.bind(this))
+  }
+  onDragged(files) {
+    let dockerfile = false
+    files.forEach(file => {
+      if(file.name === 'Dockerfile') {
+        dockerfile = true
+      }
+    })
+    this.setState({
+      files: files,
+      dockerfile: dockerfile
     })
   }
   render() {
     return (
       <div>
         <h1>Kabys</h1>
-        {this.state.files.map((file) => <div>{file.path}</div>)}
+        {this.state.dockerfile &&
+          <div>
+            This folder contains a dockerfile.
+          </div>
+        }
+        {this.state.files.map((file) => <div key={file.path}>{file.path}</div>)}
       </div>
     )
   }
