dfa121a1218c1bc051480c7e0e7d95938896b4a0
Kevin Simper <kevin.simper@gmail.com>
2015-10-09 20:36:12 +0200

    Make a button to trigger build a docker image

--- app/components/Project/index.js
+++ app/components/Project/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from './style.scss'
+import Button from '../Button'
 
 export default class Project extends React.Component {
   render() {
     return (
-      <div className={styles.Project}>{this.props.project}</div>
+      <div className={styles.Project}>
+        <div className={styles.ProjectName}>
+          {this.props.project}
+        </div>
+        <div className={styles.ProjectActions}>
+          <Button variant='blue'>Build</Button>
+        </div>
+      </div>
     )
   }
 }
