604eef94f94af4f34316934b26d4c1b85c138933
Kevin Simper <kevin.simper@gmail.com>
2015-10-09 20:35:43 +0200

    Making the button available in different colors

--- app/components/Button/index.js
+++ app/components/Button/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import './style.scss'
+import classNames from 'classnames'
+import styles from './style.scss'
 
 export default class Button extends React.Component {
   render() {
+    let classes = classNames(styles.Button, {
+      [styles.ButtonWhite]: this.props.variant === 'white' || !this.props.variant || true,
+      [styles.ButtonBlue]: this.props.variant === 'blue'
+    })
     return (
-      <button className='Button' {...this.props}>
+      <button className={classes} {...this.props}>
         {this.props.children}
       </button>
     )
