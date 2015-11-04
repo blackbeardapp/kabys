cc023850f6119f51cf9095a91c70d56280c829e5
Kevin Simper <kevin.simper@gmail.com>
2015-11-04 21:27:58 +0100

    Create a better button style generator
    As colors increase so would this stylesheet and it would be a pain to update
    so a simple sass function solves it

--- app/components/Button/index.js
+++ app/components/Button/index.js
@@ -5,10 +5,12 @@ import styles from './style.scss'
 export default class Button extends React.Component {
   render() {
     let classes = classNames(styles.Button, {
-      [styles.ButtonWhite]: this.props.variant === 'white' || !this.props.variant || true,
+      [styles.ButtonWhite]: this.props.variant === 'white' || !this.props.variant,
       [styles.ButtonBlue]: this.props.variant === 'blue',
       [styles.ButtonRed]: this.props.variant === 'red',
-      [styles.ButtonBlack]: this.props.variant === 'black'
+      [styles.ButtonBlack]: this.props.variant === 'black',
+      [styles.ButtonGreen]: this.props.variant === 'green',
+      [styles.ButtonGreenInverse]: this.props.variant === 'green-inverse',
     })
     return (
       <button className={classes} {...this.props}>
