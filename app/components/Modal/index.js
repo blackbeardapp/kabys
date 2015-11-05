67971da77b9b1b43a8e6d894bb4acb0388e929d3
Kevin Simper <kevin.simper@gmail.com>
2015-11-05 09:22:58 +0100

    Simple modal window

---
+++ app/components/Modal/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './style.scss'
+
+export default class Modal extends React.Component {
+  render() {
+    if(!this.props.display) return false;
+    
+    return (
+      <div>
+        <div className={styles.Modal}>
+          {this.props.children}
+        </div>
+        <div className={styles.Background}></div>
+      </div>
+    )
+  }
+}
