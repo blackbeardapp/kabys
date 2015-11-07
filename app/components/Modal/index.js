371cb9a4b362b1f0399de8770b20858f1a3eba57
Kevin Simper <kevin.simper@gmail.com>
2015-11-07 22:03:56 +0100

    Include svg optimisation and modal close

--- app/components/Modal/index.js
+++ app/components/Modal/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import styles from './style.scss'
+import close from './close.svg'
 
 export default class Modal extends React.Component {
+  onClickClose() {
+    this.props.onClickClose()
+  }
   render() {
     if(!this.props.show) return false;
 
     return (
       <div>
         <div className={styles.Modal}>
-          {this.props.children}
+          <div className={styles.Close} onClick={this.onClickClose.bind(this)}>
+            <img src={close}/> Close
+          </div>
+          <div>
+            {this.props.children}
+          </div>
         </div>
         <div className={styles.Background}></div>
       </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,28 @@
+var publicPath = ''
+if(process.env.NODE_ENV === 'production') {
+  publicPath = 'build/'
+} else {
+  publicPath = 'http://localhost:8080/build/'
+}
+
 module.exports = {
   entry: './app',
   output: {
     path: __dirname + '/build',
-    publicPath: '/build/',
+    publicPath: publicPath,
     filename: 'bundle.js'
   },
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-      { test: /\.scss$/, loaders: ["style", "css?localIdentName=[local]--[hash:base64:5]", "sass"]}
+      { test: /\.scss$/, loaders: ["style", "css?localIdentName=[local]--[hash:base64:5]", "sass"]},
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
     ]
   },
   externals: {
